f3f5bc76b298c3adb1da8b60ea1dd2db38d28cf8
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-09-25 11:58:50 -0600

    Need to Finish Commit

---
+++ ArrayLists of Objects Practice/src/ProgramRunner.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class ProgramRunner
+	{
+
+		public static void main(String[] args)
+			{
+				ArrayList<Program> TVShows = new ArrayList<Program>();
+
+				TVShows.add(new Program("Riverdale", "Mystery", 3));
+				TVShows.add(new Program("Grey's Anatomy", "Medical Drama", 15));
+				TVShows.add(new Program("Breaking Bad", "Drama", 5));
+				TVShows.add(new Program("House of Cards", "Political Drama", 6));
+				TVShows.add(new Program("Game of Thrones", "Fantasy", 8));
+				printList();
+				printAddYear();
+				deleteLongestTitle();
+				printNewTitle();
+
+			}
+
+		public static void printList()
+			{
+				ArrayList<Program> TVShows = new ArrayList<Program>();
+
+				TVShows.add(new Program("Riverdale", "Mystery", 3));
+				TVShows.add(new Program("Grey's Anatomy", "Medical Drama", 15));
+				TVShows.add(new Program("Breaking Bad", "Drama", 5));
+				TVShows.add(new Program("House of Cards", "Political Drama", 6));
+				TVShows.add(new Program("Game of Thrones", "Fantasy", 8));
+
+				for (Program t : TVShows)
+					{
+						System.out.println(t.getTitle());
+						System.out.println(t.getGenre());
+						System.out.println(t.getNumberOfSeasonsAired());
+
+					}
+			}
+
+		public static void printAddYear()
+			{
+
+			}
+
+		public static void deleteLongestTitle()
+			{
+
+			}
+
+		public static void printNewTitle()
+			{
+
+			}
+
+	}
